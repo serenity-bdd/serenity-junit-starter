fe9dab9b0af56173248558e79a04a8683baebbf3
John Ferguson Smart <john.smart@wakaleo.com>
2018-10-29 12:00:34 +0000

    Updated to 2.0.11

---
+++ src/test/java/starter/math/WhenAddingNumbers.java
@@ -0,0 +1,29 @@
+package starter.math;
+
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Narrative;
+import net.thucydides.core.annotations.Pending;
+import net.thucydides.core.annotations.Steps;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import starter.steps.MathWizSteps;
+
+@RunWith(SerenityRunner.class)
+@Narrative(text={"Maths is important."})
+public class WhenAddingNumbers {
+
+    @Steps
+    MathWizSteps michael;
+
+    @Test
+    public void addingSums() {
+        // Given
+        michael.startsWith(1);
+
+        // When
+        michael.adds(2);
+
+        // Then
+        michael.shouldHave(3);
+    }
+}
